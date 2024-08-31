f9036fe243649ff1c2656fd1064a11b6335812bb
DMSMain <105548836+DMSMain@users.noreply.github.com>
2024-08-31 13:16:14 +0200

    Update 4 - screens and pause menu
    - enlarged black background for main menu and options menu
    - added black background for pause menu
    - pause menu now appears and hides also on pressing Esc key

--- game_screen/game_screen.gd
+++ game_screen/game_screen.gd
@@ -1,14 +1,25 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.position = get_viewport_rect().size/2
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _input(event):
+	var PauseScreen = get_node("PauseScreen")
+	if event.is_action_pressed("ui_pause"):
+		if PauseScreen.is_visible():
+			print("paused")
+			PauseScreen.set_visible(false)
+		elif not PauseScreen.is_visible():
+			print("unpaused")
+			PauseScreen.set_visible(true)
 
 
 func _on_pause_button_pressed() -> void:
