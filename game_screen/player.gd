4ff44fe6053088d75f190d8a6ca2d3483f5f51b5
DMSMain <105548836+DMSMain@users.noreply.github.com>
2024-09-08 10:14:47 +0200

    Update 13 - player animations
    - improved the code for player movement and replaced old animations

--- game_screen/player.gd
+++ game_screen/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+# variables
 @export var speed = 400 # how fast the player will move (pixels/sec)
 var screen_size # size of the game window
 var last_move_anim # last movement animation used
@@ -12,6 +13,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	# basic four directional movement
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -22,61 +25,74 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
+	# sets idle animation
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		if last_move_anim == "e":
-			$AnimatedSprite2D.animation = "idle_w"
+			print("idling east")
+			$AnimatedSprite2D.animation = "idle_e"
 		elif last_move_anim == "ne":
-			$AnimatedSprite2D.animation = "idle_nw"
+			print("idling northeast")
+			$AnimatedSprite2D.animation = "idle_ne"
 		elif last_move_anim == "se":
-			$AnimatedSprite2D.animation = "idle_sw"
+			print("idling southeast")
+			$AnimatedSprite2D.animation = "idle_se"
 		elif last_move_anim == "w":
+			print("idling west")
 			$AnimatedSprite2D.animation = "idle_w"
 		elif last_move_anim == "nw":
+			print("idling northwest")
 			$AnimatedSprite2D.animation = "idle_nw"
 		elif last_move_anim == "sw":
+			print("idling southwest")
 			$AnimatedSprite2D.animation = "idle_sw"
 		elif last_move_anim == "s":
+			print("idling south")
 			$AnimatedSprite2D.animation = "idle_s"
 		elif last_move_anim == "n":
+			print("idling north")
 			$AnimatedSprite2D.animation = "idle_n"
 	
+	# changes player position
 	position += velocity * delta
 	
+	# sets movement animation
 	if velocity.x != 0 and velocity.y == 0:
 		if Input.is_action_pressed("move_right"):
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.animation = "move_w"
+			print("moving east")
+			$AnimatedSprite2D.animation = "move_e"
 			last_move_anim = "e"
 		if Input.is_action_pressed("move_left"):
-			$AnimatedSprite2D.flip_h = false
+			print("moving west")
 			$AnimatedSprite2D.animation = "move_w"
 			last_move_anim = "w"
 	elif velocity.x == 0 and velocity.y != 0:
 		if Input.is_action_pressed("move_down"):
+			print("moving south")
 			$AnimatedSprite2D.animation = "move_s"
 			last_move_anim = "s"
 		if Input.is_action_pressed("move_up"):
+			print("moving north")
 			$AnimatedSprite2D.animation = "move_n"
 			last_move_anim = "n"
 	elif velocity.x != 0 and velocity.y != 0:
 		if Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left"):
-			$AnimatedSprite2D.flip_h = false
+			print("moving southwest")
 			$AnimatedSprite2D.animation = "move_sw"
 			last_move_anim = "sw"
 		if Input.is_action_pressed("move_down") and Input.is_action_pressed("move_right"):
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.animation = "move_sw"
+			print("moving southeast")
+			$AnimatedSprite2D.animation = "move_se"
 			last_move_anim = "se"
 		if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
-			$AnimatedSprite2D.flip_h = false
+			print("moving northwest")
 			$AnimatedSprite2D.animation = "move_nw"
 			last_move_anim = "nw"
 		if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.animation = "move_nw"
+			print("moving northeast")
+			$AnimatedSprite2D.animation = "move_ne"
 			last_move_anim = "ne"
 
 
